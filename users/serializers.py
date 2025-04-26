eb2bb61e98eb5359f3954c8888b47f185c8f5890
vladiuse <vladiuse@gmail.com>
2025-04-26 19:27:05 +0300

    refactor

--- users/serializers.py
+++ users/serializers.py
@@ -17,20 +17,22 @@ class CustomUserRegisterSerializer(serializers.Serializer):
     password1 = serializers.CharField(write_only=True)
     password2 = serializers.CharField(write_only=True)
 
+    def create(self, validated_data: dict) -> CustomUser:
+        email = validated_data['email']
+        password = validated_data['password1']
+        return CustomUser.objects.create_user(email=email, password=password)
     def validate_email(self, value: str) -> str:
         if CustomUser.objects.filter(email=value).exists():
             raise ValidationError('User with this email already exists')
         return value
 
-    def create(self, validated_data: dict) -> CustomUser:
-        email = validated_data['email']
-        password = validated_data['password1']
-        return CustomUser.objects.create_user(email=email, password=password)
-
     def validate(self, attrs: dict) -> dict:
         if attrs['password1'] != attrs['password2']:
             raise ValidationError({'password2': 'Not equal passwords'})
-        validate_password(password=attrs['password1'])
+        try:
+            validate_password(password=attrs['password1'])
+        except DjangoValidationError as error:
+            raise ValidationError({'password1': error.messages})
         return attrs
 
 
