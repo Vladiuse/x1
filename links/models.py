52f3a1d3b52c3ce78441778e939db1752db3a0eb
vladiuse <vladiuse@gmail.com>
2025-04-27 17:39:16 +0300

    add links models

--- links/models.py
+++ links/models.py
@@ -1,1 +1,52 @@
 from django.db import models
+
+from users.models import CustomUser
+
+
+class LinkCollection(models.Model):
+    owner = models.ForeignKey(
+        to=CustomUser,
+        on_delete=models.CASCADE,
+        related_name='collections',
+        related_query_name='collection',
+    )
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=254, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    edited = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('owner', 'name')
+
+
+class Link(models.Model):
+    WEBSITE_TYPE = 'website'
+    BOOK_TYPE = 'book'
+    ARTICLE_TYPE = 'article'
+    MUSIC_TYPE = 'music'
+    VIDEO_TYPE = 'video'
+
+    PAGE_TYPES = (
+        (WEBSITE_TYPE, WEBSITE_TYPE),
+        (BOOK_TYPE, BOOK_TYPE),
+        (ARTICLE_TYPE, ARTICLE_TYPE),
+        (MUSIC_TYPE, MUSIC_TYPE),
+        (VIDEO_TYPE, VIDEO_TYPE),
+    )
+    owner = models.ForeignKey(
+        to=CustomUser,
+        on_delete=models.CASCADE,
+        related_name='links',
+        related_query_name='link',
+    )
+    url = models.URLField()
+    title = models.CharField(max_length=254, blank=True)
+    description = models.TextField(blank=True)
+    image_url = models.URLField(blank=True)
+    type = models.CharField(max_length=20, blank=True, choices=PAGE_TYPES)
+    created = models.DateTimeField(auto_now_add=True)
+    edited = models.DateTimeField(auto_now=True)
+    collections = models.ManyToManyField(LinkCollection, related_name='links', related_query_name='link')
+
+    class Meta:
+        unique_together = ('owner', 'url')
--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register('reset-password-codes', views.ResetPasswordView, basename='resetuserpasswordcode')
+router.register('reset-password-codes', views.ResetPasswordView)
 
 app_name = 'users'
 
