777fad7f5ec11c0ce4d5082f82c23957948f2526
vladiuse <vladiuse@gmail.com>
2025-04-29 20:48:44 +0300

    add domain

--- app/link_hub/settings.py
+++ app/link_hub/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '45.90.34.107']
+ALLOWED_HOSTS = ['127.0.0.1', '45.90.34.107', 'x-one.vim-store.ru']
 
 
 # Application definition
