949de7a6aad72af756809d25756e4276671a7e1d
vladiuse <vladiuse@gmail.com>
2025-04-29 21:17:02 +0300

    add libs

--- app/link_hub/settings.py
+++ app/link_hub/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django_extensions',
+    'drf_yasg',
     # apps
     'links.apps.LinksConfig',
     'users.apps.UsersConfig',
--- app/link_hub/urls.py
+++ app/link_hub/urls.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API документация",
+      default_version='v1',
+      description="Описание API вашего проекта",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@yourdomain.com"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('auth/', include('users.urls')),
     path('', include('links.urls')),
